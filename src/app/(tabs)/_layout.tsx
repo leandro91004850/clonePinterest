1ac79242e2159a3f4f25f7e05a2a7401a4c872ee
leandroleekrap <leandroleekrap@gmail.com>
2024-04-07 13:28:52 -0300

    Adicionando icons no menu

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,14 +1,34 @@
 import { Tabs } from 'expo-router'
+import * as icons from '@expo/vector-icons'
 
 export default function Layout(){
     return (
         <Tabs screenOptions={{
             headerShown: false,
+            tabBarShowLabel: false,
         }}>
-            <Tabs.Screen name="index" />
-            <Tabs.Screen name="search"/>
-            <Tabs.Screen name="profile"/>
-            <Tabs.Screen name="messages"/>
+            <Tabs.Screen 
+                name="index" 
+                options={{
+                    tabBarIcon: ({size, color}) => <icons.Foundation name="home" size={size} color={color}/>,
+                }}
+                />
+            <Tabs.Screen 
+                name="search"                
+                options={{
+                    tabBarIcon: ({size, color}) => <icons.Ionicons name="search" size={size} color={color}/>,
+                }}
+                />
+            <Tabs.Screen 
+                name="messages"                
+                options={{
+                        tabBarIcon: ({size, color}) => <icons.Ionicons name="chatbubble-ellipses" size={size} color={color}/>,
+                    }}
+                    />
+            <Tabs.Screen 
+                name="profile"
+        
+                />
         </Tabs>
     )
 }
